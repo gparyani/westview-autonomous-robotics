86461a4c3fded8da81e558f35102712629868a71
gparyani@live.com <gparyani@live.com@1893abae-4db6-8699-72c5-db63c887fb1e>
2012-11-08 03:17:51 +0000

    Fixed GyroSensorTest program.

--- year2012/GyroSensorTest.java
+++ year2012/GyroSensorTest.java
@@ -1,6 +1,7 @@
 package year2012;
-import lejos.nxt.addon.GyroSensor;
+import lejos.nxt.LCD;
 import lejos.nxt.SensorPort;
+import lejos.nxt.addon.GyroSensor;
 
 public class GyroSensorTest {
 	
@@ -9,9 +10,9 @@ public class GyroSensorTest {
 		GyroSensor sensor = new GyroSensor(SensorPort.S3);
 		while(true)
 		{
-			System.out.println(sensor.getAngularVelocity());
+			LCD.clearDisplay();
+			LCD.drawString(Float.toString(sensor.getAngularVelocity()), 0, 0);
 			Thread.sleep(50);
 		}
 	}
-
 }
