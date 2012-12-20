74a0ffd6260820f9da9f64fb4f06b5ce3f929301
edwardliang314@gmail.com <edwardliang314@gmail.com@1893abae-4db6-8699-72c5-db63c887fb1e>
2012-12-20 03:29:19 +0000

    DONT CHANGE ANYTHING YET.l OR EVEYRTHIN AOSIDFWILL DIEE> WRRNANING FRORM THT EEEE EVIVLL EOENES>S.

---
+++ maze/Map.java
@@ -0,0 +1,37 @@
+package maze;
+
+public class Map {
+	
+	private int column;
+	private int row;
+	public int rowShift;
+	public int columnShift;
+	private Tile[][] maize;
+	private int differential;
+	private int[] directions;
+	private Tile lastTile;
+	//private Robot robot;
+	public Map(int numRow, int numCol, int spacing)
+	{
+		maize = new Tile[numCol][numRow];
+		differential = spacing;
+		column = numCol;
+		row = numRow;
+		columnShift=column/2;
+		rowShift= row/2;
+		maize[rowShift][columnShift] = new Tile(row/2,column/2); 
+		lastTile = maize[rowShift][columnShift];
+		//for(int row = 0; row < numRow; row++)
+			//for(int column = 0; column < numCol; column++)
+	}
+	public void initTile(int theRow, int theCol)
+	{
+		maize[theRow+rowShift][theCol+columnShift]= new Tile(theRow,theCol, lastTile);
+		lastTile = maize[theRow+columnShift][theCol+rowShift];
+	}
+	// 
+	public void addDirection()
+	{
+		
+	}
+}
---
+++ maze/Tile.java
@@ -0,0 +1,31 @@
+package maze;
+
+public class Tile
+{
+	private int Column;
+	private int Row;
+	private Tile lastTile;
+	public Tile(int row, int column, Tile last)
+	{
+		Column = column;
+		Row = row;
+		lastTile = last;
+	}
+	public Tile(int row, int column)
+	{
+		Column = column;
+		Row = row;
+	}
+	public int getColumn()
+	{
+		return Column;
+	}
+	public int getRow()
+	{
+		return Row;
+	}
+	public Tile getLastTile()
+	{
+		return lastTile;
+	}
+}
