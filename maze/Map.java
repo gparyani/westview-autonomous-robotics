f3629376bb976e33a27889292317d3745e3f643a
jashaszun123@gmail.com <jashaszun123@gmail.com@1893abae-4db6-8699-72c5-db63c887fb1e>
2013-01-24 03:48:29 +0000

    Changed all references of 'maize' to 'maze', which I'm assuming it should be. In addition, I added spacing where someone forgot to add it.

--- maze/Map.java
+++ maze/Map.java
@@ -8,28 +8,28 @@ public class Map {
 	private int row;
 	public int rowShift;
 	public int columnShift;
-	private Tile[][] maize;
+	private Tile[][] maze;
 	private int differential;
 	private ArrayList<Integer> directions;
 	private Tile lastTile;
 	//private Robot robot;
 	public Map(int numRow, int numCol, int spacing)
 	{
-		maize = new Tile[numCol][numRow];
+		maze = new Tile[numCol][numRow];
 		differential = spacing;
 		column = numCol;
 		row = numRow;
-		columnShift=column/2;
-		rowShift= row/2;
-		maize[rowShift][columnShift] = new Tile(row/2,column/2); 
-		lastTile = maize[rowShift][columnShift];
+		columnShift = column / 2;
+		rowShift= row / 2;
+		maze[rowShift][columnShift] = new Tile(row / 2, column / 2); 
+		lastTile = maze[rowShift][columnShift];
 		//for(int row = 0; row < numRow; row++)
 			//for(int column = 0; column < numCol; column++)
 	}
 	public void initTile(int theRow, int theCol)
 	{
-		maize[theRow+rowShift][theCol+columnShift]= new Tile(theRow,theCol, lastTile);
-		lastTile = maize[theRow+columnShift][theCol+rowShift];
+		maze[theRow + rowShift][theCol + columnShift]= new Tile(theRow, theCol, lastTile);
+		lastTile = maze[theRow + columnShift][theCol + rowShift];
 	}
 	// 
 	public void addDirection(int direction)
