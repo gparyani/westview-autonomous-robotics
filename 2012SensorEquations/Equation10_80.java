2940c4ee77b40a7957e80a5c6aaf8af9be686b31
feralinprog@gmail.com <feralinprog@gmail.com@1893abae-4db6-8699-72c5-db63c887fb1e>
2013-01-31 02:57:55 +0000

    Added comments.

--- 2012SensorEquations/Equation10_80.java
+++ 2012SensorEquations/Equation10_80.java
@@ -4,8 +4,11 @@ public class Equation10_80 {
 		//test
 		System.out.println(function(900));
 	}
+	
+	//  terribly descriptive function name...
 	public static double function(int voltage)
 	{
+		// gaahhh! MAGIC CONSTANTS! RUN FOR YOUR LIVES!!!!
 		return 52650.9/((Math.pow((double)voltage,1.274715401143982)));
 	}
 }
