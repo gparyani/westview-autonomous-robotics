1fce345f92510136eede2c054bfb0d8254247507
feralinprog@gmail.com <feralinprog@gmail.com@1893abae-4db6-8699-72c5-db63c887fb1e>
2013-01-31 03:45:19 +0000

    Added comments.

--- year2012/MotorRotateTest.java
+++ year2012/MotorRotateTest.java
@@ -13,6 +13,8 @@ public class MotorRotateTest
 	private static final int 
 		MOTOR_LEFT = TetrixMotorController.MOTOR_1,
 		MOTOR_RIGHT = TetrixMotorController.MOTOR_2;
+	private static final int
+		UPDATE_WAIT = 25; // ms
 	
 	private static TetrixEncoderMotor getMotor(int motorID)
 	{
@@ -21,23 +23,32 @@ public class MotorRotateTest
 	
 	public static void main(String[] args) throws Exception
 	{
+		LCD.drawString("Hello, NXT!", 5, 5);
+		
 		TetrixControllerFactory factory = new TetrixControllerFactory(SensorPort.S1);
 		controller = factory.newMotorController();
+		
+		// right motor goes in wrong direction...
+		getMotor(MOTOR_RIGHT).setReverse(true);
 
+		// until ESCAPE button is pressed...
 		while (Button.ESCAPE.isUp())
 		{
 			if (Button.LEFT.isDown())
 			{
+				// rotate left: LEFT backward, RIGHT forward
 				getMotor(MOTOR_LEFT).backward();
 				getMotor(MOTOR_RIGHT).forward();
 			}
 			else if (Button.RIGHT.isDown())
 			{
+				// rotate right: LEFT forward, RIGHT backward
 				getMotor(MOTOR_LEFT).forward();
 				getMotor(MOTOR_RIGHT).backward();
 			}
 			
-			Thread.sleep(500);
+			// wait for next loop
+			Thread.sleep(UPDATE_WAIT);
 		}
 	}
 }
