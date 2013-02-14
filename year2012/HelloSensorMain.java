ea9ebf4ed5e8b9e3b9beab898ed6eb937b90ec57
feralinprog@gmail.com <feralinprog@gmail.com@1893abae-4db6-8699-72c5-db63c887fb1e>
2013-02-14 02:34:28 +0000

    Added HelloSensorMain sensor testing class. (Dave Craig's code)

---
+++ year2012/HelloSensorMain.java
@@ -0,0 +1,49 @@
+package year2012;
+
+import lejos.nxt.Button;
+import lejos.nxt.I2CPort;
+import lejos.nxt.I2CSensor;
+import lejos.nxt.LCD;
+import lejos.nxt.SensorPort;
+
+public class HelloSensorMain extends I2CSensor {
+
+	private static final int READ_BUFFER_ADDRESS = 0x42;
+	private static final int SUPERPRO_ADDRESS = 0x10;
+
+	public HelloSensorMain(I2CPort port)
+	{
+		super(port, SUPERPRO_ADDRESS, I2CPort.LEGO_MODE, TYPE_LOWSPEED_9V);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		HelloSensorMain hsm = new HelloSensorMain(SensorPort.S1);
+		byte[] readBuffer = new byte[2];
+		int accum = 0;
+		while (!Button.ESCAPE.isDown())
+		{
+			try
+			{
+				Thread.sleep(50);
+			} 
+			catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+			hsm.getData(READ_BUFFER_ADDRESS, readBuffer, 2);
+			int inputData = (0xff & (int) readBuffer[0]) * 4
+					+ (0xff & (int) readBuffer[1]);
+			accum = accum / 2 + inputData;
+			LCD.clearDisplay();
+			LCD.drawInt(inputData, 0, 0);
+			LCD.drawInt(readBuffer[0], 1, 1);
+			LCD.drawInt(readBuffer[1], 2, 2);
+			LCD.drawInt(accum / 2, 3, 3);
+		}
+	}
+
+}
