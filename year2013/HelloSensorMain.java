dc029634c9d8aaf16dbdc32f315aa00ed060c9be
jashaszun123@gmail.com <jashaszun123@gmail.com@1893abae-4db6-8699-72c5-db63c887fb1e>
2013-02-19 20:45:58 +0000

    Made HelloSensorMain read and display data from all three sensors (A0, A1, B) (2 Analog + 1 Digital, respectively).

--- year2013/HelloSensorMain.java
+++ year2013/HelloSensorMain.java
@@ -8,7 +8,9 @@ import lejos.nxt.SensorPort;
 
 public class HelloSensorMain extends I2CSensor {
 
-	private static final int READ_BUFFER_ADDRESS = 0x42;
+	private static final int ADDR_A0 = 0x42;
+	private static final int ADDR_A1 = 0x44;
+	private static final int ADDR_B = 0x4C;
 	private static final int SUPERPRO_ADDRESS = 0x10;
 
 	public HelloSensorMain(I2CPort port)
@@ -21,11 +23,15 @@ public class HelloSensorMain extends I2CSensor {
 	 */
 	public static void main(String[] args)
 	{
-		HelloSensorMain hsm = new HelloSensorMain(SensorPort.S1);
-		byte[] readBuffer = new byte[2];
-		int accum = 0;
+		HelloSensorMain sensors = new HelloSensorMain(SensorPort.S2);
 		while (!Button.ESCAPE.isDown())
 		{
+			UpdateA0(sensors);
+			UpdateA1(sensors);
+			UpdateB(sensors);
+			
+			DisplayData();
+			
 			try
 			{
 				Thread.sleep(50);
@@ -34,16 +40,43 @@ public class HelloSensorMain extends I2CSensor {
 			{
 				e.printStackTrace();
 			}
-			hsm.getData(READ_BUFFER_ADDRESS, readBuffer, 2);
-			int inputData = (0xff & (int) readBuffer[0]) * 4
-					+ (0xff & (int) readBuffer[1]);
-			accum = accum / 2 + inputData;
-			LCD.clearDisplay();
-			LCD.drawString("Input:   " + inputData, 0, 0);
-			LCD.drawString("Read[0]: " + readBuffer[0], 0, 1);
-			LCD.drawString("Read[1]: " + readBuffer[1], 0, 2);
-			LCD.drawString("Accum/2: " + accum / 2, 0, 3);
 		}
 	}
 
+	static int accum0 = 0, accum1 = 0;
+	static boolean b0 = false;
+	static void UpdateA0(HelloSensorMain sensors)
+	{
+		byte[] a0_buffer = new byte[2];
+		
+		sensors.getData(ADDR_A0, a0_buffer, 2);
+		int inputData = (0xff & (int)a0_buffer[0]) * 4
+				+ (0xff & (int)a0_buffer[1]);
+		accum0 = accum0 / 2 + inputData;
+	}
+	static void UpdateA1(HelloSensorMain sensors)
+	{
+		byte[] a1_buffer = new byte[2];
+		
+		sensors.getData(ADDR_A1, a1_buffer, 2);
+		int inputData = (0xff & (int)a1_buffer[0]) * 4
+				+ (0xff & (int)a1_buffer[1]);
+		accum1 = accum1 / 2 + inputData;
+	}
+	static void UpdateB(HelloSensorMain sensors)
+	{
+		byte[] shortRangeReadBuffer = new byte[1];
+
+		sensors.getData(ADDR_B, shortRangeReadBuffer, 1);
+		b0 = (shortRangeReadBuffer[0] & 0x01) == 0;
+	}
+	
+	static void DisplayData()
+	{
+		LCD.clearDisplay();
+		
+		LCD.drawString("Accum0: " + accum0, 0, 0);
+		LCD.drawString("Accum1: " + accum1, 0, 1);
+		LCD.drawString("SR Read: " + b0, 0, 2);
+	}
 }
