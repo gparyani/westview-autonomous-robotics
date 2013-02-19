b87b5963b7bfbe678e1b470c8a52971e44771797
jashaszun123@gmail.com <jashaszun123@gmail.com@1893abae-4db6-8699-72c5-db63c887fb1e>
2013-02-19 21:31:24 +0000

    Added SensorAddresses, which simply holds common addresses for sensors and the superpro.

---
+++ year2013/NXTApp/SensorAddresses.java
@@ -0,0 +1,11 @@
+package year2013.NXTApp;
+
+public class SensorAddresses
+{
+	public static final int Superpro = 0x10;
+	public static final int A0 = 0x42,
+							A1 = 0x44,
+							A2 = 0x46,
+							A3 = 0x48,
+							B = 0x4C;
+}
