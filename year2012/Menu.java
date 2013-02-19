f9744173a0e4a6913f398d84c938a66a736dd38f
feralinprog@gmail.com <feralinprog@gmail.com@1893abae-4db6-8699-72c5-db63c887fb1e>
2013-02-19 18:46:45 +0000

    Added comments to class.

--- year2012/Menu.java
+++ year2012/Menu.java
@@ -3,6 +3,7 @@ package year2012;
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
 
+// a simple menu system
 public class Menu
 {
 	private Menu() throws Exception
@@ -10,9 +11,12 @@ public class Menu
 		throw new Exception("Cannot create Menu instance!");
 	}
 	
+	// shows a menu, and waits for the user to select an option
 	public static int show(String title, String... options)
 	{
+		// draw title
 		LCD.drawString(title, 0, 0);
+		// and all the options
 		for (int opt = 0; opt < options.length; opt++)
 			LCD.drawString(options[opt], opt + 1, 1);
 
@@ -22,11 +26,17 @@ public class Menu
 		while (true)
 		{
 			if (Button.RIGHT.isDown())
+				// right -> select choice
+				// choice is 1-based, so subtract 1 to make it 0-based
 				return choice - 1;
 			else if (Button.LEFT.isDown())
+				// left -> exit/quit menu
+				// -1 is error code
 				return -1;
 			else if (Button.ENTER.isDown())
 			{
+				// enter -> move pointer up
+				// wraps around top to bottom
 				int prev = choice;
 				choice--;
 				if (choice < 1)
@@ -35,6 +45,8 @@ public class Menu
 			}
 			else if (Button.ESCAPE.isDown())
 			{
+				// escape -> move pointer down
+				// wraps around bottom to top
 				int prev = choice;
 				choice++;
 				if (choice > options.length)
@@ -51,6 +63,7 @@ public class Menu
 			}
 		}
 	}
+	// helper function to rewrite selection pointer
 	private static void move(int prev, int cur)
 	{
 		LCD.drawChar(' ', 0, prev);
