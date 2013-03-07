07fdcde509dba7ae52e87a6f0d58bacb42ab5618
feralinprog@gmail.com <feralinprog@gmail.com@1893abae-4db6-8699-72c5-db63c887fb1e>
2013-03-07 03:19:54 +0000

    Modified again.

--- year2013/SensorTest.java
+++ year2013/SensorTest.java
@@ -13,12 +13,14 @@ public class SensorTest extends NXTApp
 	{
 		super(50);
 		
-		LookupTable.LoadSensorData();
+		//LookupTable.LoadSensorData();
 		
 		A0 = new DistanceSensor(SensorAddresses.A0, SensorAddresses.Superpro, SensorPort.S2);
 		A1 = new DistanceSensor(SensorAddresses.A1, SensorAddresses.Superpro, SensorPort.S2);
 		B = new DigitalSensor(SensorAddresses.B, 0, SensorAddresses.Superpro, SensorPort.S2);
 	}
+	
+	int maxA0 = -1, maxA1 = -1;
 	
 	protected void Update()
 	{
@@ -26,12 +28,21 @@ public class SensorTest extends NXTApp
 		A1.Update();
 		B.Update();
 		
+		if (A0.GetVoltage() > maxA0)
+			maxA0 = A0.GetVoltage();
+		if (A1.GetVoltage() > maxA1)
+			maxA1 = A1.GetVoltage();
+		
 		LCD.clearDisplay();
 		LCD.drawString("Accum0: " + A0.GetVoltage(), 0, 0);
 		LCD.drawString("Accum1: " + A1.GetVoltage(), 0, 1);
 		LCD.drawString("SR Read: " + B.GetData(), 0, 2);
+		if (maxA0 != -1)
+			LCD.drawString("Max Accum0: " + maxA0, 0, 4);
+		if (maxA1 != -1)
+			LCD.drawString("Max Accum1: " + maxA1, 0, 5);
 		
-		LCD.drawString("Distance: " + DistanceSensor.GetDistance(A0, A1, B), 0, 4);
+		//LCD.drawString("Distance: " + DistanceSensor.GetDistance(A0, A1, B), 0, 6);
 	}
 	
 	protected boolean ShouldExit()
