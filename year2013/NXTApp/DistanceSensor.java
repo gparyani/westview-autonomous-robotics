ab6f545cc08476388767f512870b3086023a0f0a
jashaszun123@gmail.com <jashaszun123@gmail.com@1893abae-4db6-8699-72c5-db63c887fb1e>
2013-03-07 03:40:15 +0000

    Made DistanceSensor load two LookupTables that contain established medium-range and long-range data, where each datum is a key-value pair of distance (in cm) and voltage.

--- year2013/NXTApp/DistanceSensor.java
+++ year2013/NXTApp/DistanceSensor.java
@@ -1,6 +1,7 @@
 package year2013.NXTApp;
 
 import year2013.LookupTable;
+import year2013.MapEntry;
 import lejos.nxt.SensorPort;
 
 public class DistanceSensor extends Sensor
@@ -27,6 +28,63 @@ public class DistanceSensor extends Sensor
 		return this.Voltage;
 	}
 	
+	private static LookupTable MediumRangeData, LongRangeData;
+	private static String[] MediumRangeReadings = new String[]
+	{
+		"5	846",
+		"10	811.5",
+		"15	505",
+		"20	343.3333333",
+		"25	283.6666667",
+		"30	257.8333333",
+		"35	237.5",
+		"40	239.5",
+		"45	237.5",
+		"50	243.8333333",
+		"55	240.3333333",
+		"60	245.1666667",
+		"65	242.6666667",
+		"70	235.5",
+		"75	237.8333333",
+		"80	233.6666667",
+		"85	235.3333333"
+	};
+	private static String[] LongRangeReadings = new String[]
+	{
+		"20	678.3333333",
+		"30	559",
+		"40	411",
+		"50	324",
+		"60	265",
+		"70	236.3333333",
+		"80	228",
+		"90	203.3333333",
+		"100 181.6666667",
+		"110 167.3333333",
+		"120 164",
+		"130 169",
+		"140 172.3333333",
+		"150 165"
+	};
+
+	
+	public static void LoadSensorData()
+	{
+		MediumRangeData = new LookupTable();
+		LongRangeData = new LookupTable();
+		for (int i = 0; i < MediumRangeReadings.length; i++)
+			MediumRangeData.set(getEntry(MediumRangeReadings[i]));
+		for (int i = 0; i < LongRangeReadings.length; i++)
+			LongRangeData.set(getEntry(LongRangeReadings[i]));
+	}
+	private static MapEntry<Double, Double> getEntry(String line)
+	{
+		double distance = Double.parseDouble(line);
+		String afterDistance = line.substring(((Double)distance).toString().length());
+		double voltage = Double.parseDouble(afterDistance);
+		return new MapEntry<Double, Double>(voltage, distance);
+	}
+	
 	// return value is in cm
 	public static double GetDistance(DistanceSensor longRangeSensor, DistanceSensor mediumRangeSensor, DigitalSensor shortRangeSensor)
 	{
@@ -35,8 +93,8 @@ public class DistanceSensor extends Sensor
 		double longRange = longRangeSensor.GetVoltage();
 		
 		if (shortRange)
-			return LookupTable.MediumRangeData.get(mediumRange);
+			return MediumRangeData.get(mediumRange);
 		else
-			return LookupTable.LongRangeData.get(longRange);
+			return LongRangeData.get(longRange);
 	}
 }
