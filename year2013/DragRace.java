a1f200af80b1eb697e059617f2fa1e62a9bce1ff
feralinprog@gmail.com <feralinprog@gmail.com@1893abae-4db6-8699-72c5-db63c887fb1e>
2013-03-21 01:27:34 +0000

    Radically simplified the drag race to not use any sensors; it simply keeps running.

--- year2013/DragRace.java
+++ year2013/DragRace.java
@@ -6,11 +6,11 @@ import lejos.nxt.SensorPort;
 
 public class DragRace extends NXTApp
 {
-	DigitalSensor ShortRange;
-	DistanceSensor MediumRange;
-	boolean Exit = false;
+	//DigitalSensor ShortRange;
+	//DistanceSensor MediumRange;
+	//boolean Exit = false;
 	boolean Running = false;
-	boolean WaitingToExit = false;
+	//boolean WaitingToExit = false;
 	
 	public DragRace()
 	{
@@ -18,15 +18,15 @@ public class DragRace extends NXTApp
 		
 		Motors.Initialize(SensorPort.S1);
 		
-		ShortRange = new DigitalSensor(SensorAddresses.B, 0, SensorAddresses.Superpro, SensorPort.S2);
-		MediumRange = new DistanceSensor(SensorAddresses.A1, SensorAddresses.Superpro, SensorPort.S2);
+		//ShortRange = new DigitalSensor(SensorAddresses.B, 0, SensorAddresses.Superpro, SensorPort.S2);
+		//MediumRange = new DistanceSensor(SensorAddresses.A1, SensorAddresses.Superpro, SensorPort.S2);
 	}
 	
 	protected void Update()
 	{
 		if (Running)
 			LCD.drawString("Drag-Race\nin Progress.", 0, 0);
-		else if (!WaitingToExit && !Exit)
+		else// if (!WaitingToExit && !Exit)
 		{
 			LCD.drawString("Press any button\nto start the\ndrag-race.", 0, 0);
 
@@ -43,34 +43,34 @@ public class DragRace extends NXTApp
 		}
 		
 		
-		ShortRange.Update();
-		MediumRange.Update();
-		LCD.drawString("Short-Range: " + ((Boolean)ShortRange.GetData()).toString().charAt(0), 0, 6);
-		LCD.drawString("Medium-Range: " + MediumRange.GetVoltage(), 0, 7);
-		if (Running && ShortRange.GetData())
-		{
-			Motors.Left.stop();
-			Motors.Right.stop();
-			Motors.Front.stop();
-			Motors.Back.stop();
-			WaitingToExit = true;
-			Running = false;
-		}
+		//ShortRange.Update();
+		//MediumRange.Update();
+		//LCD.drawString("Short-Range: " + ((Boolean)ShortRange.GetData()).toString().charAt(0), 0, 6);
+		//LCD.drawString("Medium-Range: " + MediumRange.GetVoltage(), 0, 7);
+		//if (Running && ShortRange.GetData())
+		//{
+		//	Motors.Left.stop();
+		//	Motors.Right.stop();
+		//	Motors.Front.stop();
+		//	Motors.Back.stop();
+		//	WaitingToExit = true;
+		//	Running = false;
+		//}
 		
-		if (WaitingToExit)
-		{
-			LCD.drawString("Press any button\nto exit.", 0, 0);
-			if (Button.Left.IsDown() || Button.Right.IsDown() 
-					|| Button.Enter.IsDown() || Button.Escape.IsDown())
-			{
-				WaitingToExit = false;
-				Exit = true;
-			}
-		}
+		//if (WaitingToExit)
+		//{
+		//	LCD.drawString("Press any button\nto exit.", 0, 0);
+		//	if (Button.Left.IsDown() || Button.Right.IsDown() 
+		//			|| Button.Enter.IsDown() || Button.Escape.IsDown())
+		//	{
+		//		WaitingToExit = false;
+		//		Exit = true;
+		//	}
+		//}
 	}
 	protected boolean ShouldExit()
 	{
-		return Exit;
+		return false;//Exit;
 	}
 	
 	public static void main(String[] args) throws Exception
