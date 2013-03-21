e31279c5f1181a35eb506be6b7b89166e02193e8
jashaszun123@gmail.com <jashaszun123@gmail.com@1893abae-4db6-8699-72c5-db63c887fb1e>
2013-03-21 01:06:28 +0000

    Created a new package made specifically for test programs, and added a new test to it: CompassSensorTest!

---
+++ year2013/Tests/CompassSensorTest.java
@@ -0,0 +1,29 @@
+package year2013.Tests;
+
+import year2013.NXTApp.NXTApp;
+import lejos.nxt.LCD;
+import lejos.nxt.SensorPort;
+import year2013.NXTApp.CompassSensor;
+
+public class CompassSensorTest extends NXTApp
+{
+	CompassSensor Compass;
+	
+	public CompassSensorTest()
+	{
+		super(50);
+		
+		Compass = new CompassSensor(SensorPort.S3);
+	}
+	
+	protected void Update()
+	{
+		int degree = Compass.GetAngle();
+		LCD.drawString("Deg: " + degree, 0, 0);
+	}
+	
+	protected boolean ShouldExit()
+	{
+		return false;
+	}
+}
