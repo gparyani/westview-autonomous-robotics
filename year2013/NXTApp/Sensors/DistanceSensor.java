aa238cefff7c31e928b4946c0a3c4c04da58b5d4
feralinprog@gmail.com <feralinprog@gmail.com@1893abae-4db6-8699-72c5-db63c887fb1e>
2013-03-26 19:58:59 +0000

    Added calibration ability to DistanceSensor.

--- year2013/NXTApp/Sensors/DistanceSensor.java
+++ year2013/NXTApp/Sensors/DistanceSensor.java
@@ -1,5 +1,7 @@
 package year2013.NXTApp.Sensors;
 
+import year2013.FitLine;
+import year2013.LeastSquares;
 import lejos.nxt.SensorPort;
 
 public class DistanceSensor extends Sensor
@@ -29,63 +31,25 @@ public class DistanceSensor extends Sensor
 		return this.Voltage;
 	}
 	
-//	private static LookupTable MediumRangeData, LongRangeData;
-//	private static String[] MediumRangeReadings = new String[]
-//	{
-//		"5	846",
-//		"10	811.5",
-//		"15	505",
-//		"20	343.3333333",
-//		"25	283.6666667",
-//		"30	257.8333333",
-//		"35	237.5",
-//		"40	239.5",
-//		"45	237.5",
-//		"50	243.8333333",
-//		"55	240.3333333",
-//		"60	245.1666667",
-//		"65	242.6666667",
-//		"70	235.5",
-//		"75	237.8333333",
-//		"80	233.6666667",
-//		"85	235.3333333"
-//	};
-//	private static String[] LongRangeReadings = new String[]
-//	{
-//		"20	678.3333333",
-//		"30	559",
-//		"40	411",
-//		"50	324",
-//		"60	265",
-//		"70	236.3333333",
-//		"80	228",
-//		"90	203.3333333",
-//		"100 181.6666667",
-//		"110 167.3333333",
-//		"120 164",
-//		"130 169",
-//		"140 172.3333333",
-//		"150 165"
-//	};
-
-	
-//	public static void LoadSensorData()
-//	{
-//		MediumRangeData = new LookupTable();
-//		LongRangeData = new LookupTable();
-//		for (int i = 0; i < MediumRangeReadings.length; i++)
-//			MediumRangeData.set(getEntry(MediumRangeReadings[i]));
-//		for (int i = 0; i < LongRangeReadings.length; i++)
-//			LongRangeData.set(getEntry(LongRangeReadings[i]));
-//	}
-//	private static MapEntry<Double, Double> getEntry(String line)
-//	{
-//		double distance = Double.parseDouble(line);
-//		String afterDistance = line.substring(((Double)distance).toString().length());
-//		double voltage = Double.parseDouble(afterDistance);
-//		return new MapEntry<Double, Double>(voltage, distance);
-//	}
-	
+	private static double 
+		calibrationMult = 6800, 
+		calibrationOffset = 0;
+	// takes a set of voltage levels,
+	// where voltages[i] is the voltage at distance = i*10 cm
+	// and then calibrates for optimal indicated distance results
+	public static void calibrate(double[] voltages)
+	{
+		double[] xs = new double[voltages.length],
+				 ys = new double[voltages.length];
+		for (int i = 0; i < voltages.length; i++)
+		{
+			xs[i] = 10 * (i + 1);
+			ys[i] = 1 / voltages[i];
+		}
+		FitLine line = LeastSquares.fit(xs, voltages);
+		calibrationMult = line.slope;
+		calibrationOffset = line.offset;
+	}
 	// return value is in cm
 	public static double GetDistance(/*DistanceSensor longRangeSensor, */DistanceSensor mediumRangeSensor, DigitalSensor shortRangeSensor)
 	{
@@ -93,13 +57,11 @@ public class DistanceSensor extends Sensor
 		double mediumRange = mediumRangeSensor.GetVoltage();
 		//double longRange = longRangeSensor.GetVoltage();
 		
-		final double CALIBRATION_MULT = 6800;
-		final double CALIBRATION_OFFSET = 0;
-		
 		if (shortRange)
 			return 0;//MediumRangeData.get(mediumRange);
 		else
-			return CALIBRATION_MULT / mediumRange + CALIBRATION_OFFSET;//LongRangeData.get(longRange);
+			return calibrationMult / mediumRange + calibrationOffset;
+				//LongRangeData.get(longRange);
 		// take a look at http://www.pololu.com/file/download/gp2y0a21yk0f.pdf?file_id=0J85
 	}
 }
