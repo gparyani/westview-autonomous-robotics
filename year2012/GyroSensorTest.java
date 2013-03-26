cce7ecd343b17580ca965fc44f251c568821f02a
gparyani@live.com <gparyani@live.com@1893abae-4db6-8699-72c5-db63c887fb1e>
2013-03-26 18:07:15 +0000

    Use GyroDirectionFinder class instead of getting readings from the sensor directly.

--- year2012/GyroSensorTest.java
+++ year2012/GyroSensorTest.java
@@ -2,18 +2,19 @@ package year2012;
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
 import lejos.nxt.SensorPort;
+import lejos.nxt.addon.GyroDirectionFinder;
 import lejos.nxt.addon.GyroSensor;
 
 public class GyroSensorTest {
 	
 	public static void main(String... args) throws Exception
 	{
-		GyroSensor sensor = new GyroSensor(SensorPort.S3);
-		while(true)
+		GyroDirectionFinder finder = new GyroDirectionFinder(new GyroSensor(SensorPort.S1), true);
+		finder.resetCartesianZero();
+		while((Button.ENTER.isUp() && Button.ESCAPE.isUp()))
 		{
 			LCD.clearDisplay();
-			LCD.drawString("" + (sensor.getAngularVelocity() - 1023f), 0, 0);
-			if (Button.ENTER.isDown() || Button.ESCAPE.isDown()) break;
+			LCD.drawString(String.valueOf(finder.getAngularVelocity()), 0, 0);
 			Thread.sleep(50);
 		}
 	}
