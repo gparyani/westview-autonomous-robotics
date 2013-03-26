624ac898af799d6ea5df7423b0bcae4443fb85df
feralinprog@gmail.com <feralinprog@gmail.com@1893abae-4db6-8699-72c5-db63c887fb1e>
2013-03-26 19:57:43 +0000

    FitLine class for the least squares algorithm

---
+++ year2013/FitLine.java
@@ -0,0 +1,13 @@
+package year2013;
+
+//represents the slope and offset of a best-fit line
+public class FitLine
+{
+	public double slope, offset;
+	
+	public FitLine(double slope, double offset)
+	{
+		this.slope = slope;
+		this.offset = offset;
+	}
+}
