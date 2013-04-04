7d440fc2bfd0d04168c8149a48dc2dcade758b6d
jashaszun123@gmail.com <jashaszun123@gmail.com@1893abae-4db6-8699-72c5-db63c887fb1e>
2013-04-04 01:02:46 +0000

    Added two new Tests: DragRaceGyroTest and MotorPowerTest.

---
+++ year2013/Tests/DragRaceGyroTest.java
@@ -0,0 +1,88 @@
+package year2013.Tests;
+
+import year2013.NXTApp.*;
+import year2013.NXTApp.Sensors.*;
+import lejos.nxt.LCD;
+import lejos.nxt.SensorPort;
+
+public class DragRaceGyroTest extends NXTApp
+{
+	boolean Started = false;
+	boolean Exit = false;
+	
+	DigitalSensor ShortRange;
+	DistanceSensor MediumRange;
+	GyroSensor Gyro;
+	
+	public DragRaceGyroTest()
+	{
+		super(50);
+		
+		Motors.Initialize(SensorPort.S1);
+		
+		ShortRange = new DigitalSensor(SensorAddresses.B, 0, SensorAddresses.Superpro, SensorPort.S2);
+		MediumRange = new DistanceSensor(SensorAddresses.A1, SensorAddresses.Superpro, SensorPort.S2);
+		Gyro = new GyroSensor(SensorPort.S3);
+	}
+	
+	protected void Update()
+	{
+		ShortRange.Update();
+		MediumRange.Update();
+		Gyro.Update();
+
+		LCD.clearDisplay();
+		LCD.drawString("Angle: " + Gyro.GetAngle(), 0, 6);
+		if (!Started)
+		{
+			LCD.drawString("Press any button\nto start the\ndrag race.", 0, 0);
+			Gyro.ResetAngle();
+
+			if (Button.Left.Pressed() || Button.Right.Pressed() 
+					|| Button.Enter.Pressed() || Button.Escape.Pressed())
+			{
+				Motors.Left.backward();
+				Motors.Right.forward();
+				Motors.Front.stop();
+				Motors.Back.stop();
+				LCD.clearDisplay();
+				Started = true;
+			}
+			
+			LCD.drawString("Dist: " + DistanceSensor.GetDistance(MediumRange, ShortRange) + "cm", 0, 7);
+		}
+		else
+		{
+			LCD.drawString("Press any button\nto end the race.", 0, 0);
+			
+			UpdateMotors();
+			
+			if (//DistanceSensor.GetDistance(MediumRange, ShortRange) < 20
+					/*||*/ Button.Left.Pressed() || Button.Right.Pressed() 
+					|| Button.Enter.Pressed() || Button.Escape.Pressed())
+			{
+				Motors.Left.stop();
+				Motors.Right.stop();
+				Motors.Front.stop();
+				Motors.Back.stop();
+				Exit = true;
+			}
+		}
+	}
+	
+	private void UpdateMotors()
+	{
+		double angle = Gyro.GetAngle();
+		Motors.Front.setPower((int)angle);
+	}
+	
+	protected boolean ShouldExit()
+	{
+		return Exit;
+	}
+	
+	public static void main(String[] args) throws Exception
+	{
+		new DragRaceGyroTest().Run();
+	}
+}
---
+++ year2013/Tests/MotorPowerTest.java
@@ -0,0 +1,52 @@
+package year2013.Tests;
+import lejos.nxt.LCD;
+import lejos.nxt.SensorPort;
+import year2013.NXTApp.*;
+
+public class MotorPowerTest extends NXTApp
+{
+	private int PowerPercent;
+	
+	public MotorPowerTest()
+	{
+		super(50);
+		PowerPercent = 0;
+		
+		Motors.Initialize(SensorPort.S1);
+		
+		Motors.Front.forward();
+		Motors.Back.forward();
+		Motors.Left.forward();
+		Motors.Right.forward();
+	}
+
+	protected void Update()
+	{
+		LCD.clearDisplay();
+		LCD.drawString("% Power: " + this.PowerPercent, 0, 0);
+		
+		if (Button.Left.IsDown())
+			this.PowerPercent--;
+		if (Button.Right.IsDown())
+			this.PowerPercent++;
+		if (this.PowerPercent < 0)
+			this.PowerPercent = 0;
+		if (this.PowerPercent > 100)
+			this.PowerPercent = 100;
+		
+		Motors.Front.setPower(this.PowerPercent);
+		Motors.Back.setPower(this.PowerPercent);
+		Motors.Left.setPower(this.PowerPercent);
+		Motors.Right.setPower(this.PowerPercent);
+	}
+	protected boolean ShouldExit()
+	{
+		return Button.Escape.IsDown();
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		new MotorPowerTest().Run();
+	}
+}
