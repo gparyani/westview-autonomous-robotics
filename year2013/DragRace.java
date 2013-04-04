d2bd2e695c05aafa428aeaa9443482fb08b90bed
jashaszun123@gmail.com <jashaszun123@gmail.com@1893abae-4db6-8699-72c5-db63c887fb1e>
2013-04-04 01:44:57 +0000

    Moved the code in DragRaceGyroTest to DragRace. The test is complete! Course correction seems to be working, but sometimes the GyroSensor seems to suddenly get an offset in its angle.

--- year2013/DragRace.java
+++ year2013/DragRace.java
@@ -12,38 +12,31 @@ public class DragRace extends NXTApp
 	
 	DigitalSensor ShortRange;
 	DistanceSensor MediumRange;
+	GyroSensor Gyro;
 	
 	public DragRace()
 	{
-		super(0);
-		
-		DistanceSensor.calibrate(new double[] {
-			13.5,
-			28,
-			44,
-			58,
-			74,
-			91,
-			110,
-			105,
-			133,
-			155
-		});
+		super(50);
 		
 		Motors.Initialize(SensorPort.S1);
 		
 		ShortRange = new DigitalSensor(SensorAddresses.B, 0, SensorAddresses.Superpro, SensorPort.S2);
 		MediumRange = new DistanceSensor(SensorAddresses.A1, SensorAddresses.Superpro, SensorPort.S2);
+		Gyro = new GyroSensor(SensorPort.S3);
 	}
 	
 	protected void Update()
 	{
 		ShortRange.Update();
 		MediumRange.Update();
-		
+		Gyro.Update();
+
+		LCD.clearDisplay();
+		LCD.drawString("Angle: " + Gyro.GetAngle(), 0, 6);
 		if (!Started)
 		{
 			LCD.drawString("Press any button\nto start the\ndrag race.", 0, 0);
+			Gyro.ResetAngle();
 
 			if (Button.Left.Pressed() || Button.Right.Pressed() 
 					|| Button.Enter.Pressed() || Button.Escape.Pressed())
@@ -55,12 +48,17 @@ public class DragRace extends NXTApp
 				LCD.clearDisplay();
 				Started = true;
 			}
+			
+			LCD.drawString("Dist: " + DistanceSensor.GetDistance(MediumRange, ShortRange) + "cm", 0, 7);
 		}
 		else
 		{
 			LCD.drawString("Press any button\nto end the race.", 0, 0);
-			if (DistanceSensor.GetDistance(MediumRange, ShortRange) < 20
-					|| Button.Left.Pressed() || Button.Right.Pressed() 
+			
+			UpdateMotors();
+			
+			if (//DistanceSensor.GetDistance(MediumRange, ShortRange) < 20
+					/*||*/ Button.Left.Pressed() || Button.Right.Pressed() 
 					|| Button.Enter.Pressed() || Button.Escape.Pressed())
 			{
 				Motors.Left.stop();
@@ -71,6 +69,14 @@ public class DragRace extends NXTApp
 			}
 		}
 	}
+	
+	private void UpdateMotors()
+	{
+		double angle = Gyro.GetAngle();
+		Motors.Front.setPower((int)angle);
+		Motors.Back.setPower((int)angle);
+	}
+	
 	protected boolean ShouldExit()
 	{
 		return Exit;
