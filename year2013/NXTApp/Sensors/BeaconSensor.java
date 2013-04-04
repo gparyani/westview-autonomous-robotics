def7fa0b7f04e0aad2cdd9bf5ffdaccd3dbcba97
feralinprog@gmail.com <feralinprog@gmail.com@1893abae-4db6-8699-72c5-db63c887fb1e>
2013-04-04 02:21:27 +0000

    Modified BeaconSensor to internally use lejos.nxt.addon.IRSeekerV2.

--- year2013/NXTApp/Sensors/BeaconSensor.java
+++ year2013/NXTApp/Sensors/BeaconSensor.java
@@ -1,46 +1,44 @@
 package year2013.NXTApp.Sensors;
 
 import lejos.nxt.SensorPort;
+import lejos.nxt.addon.*;
+import lejos.nxt.addon.IRSeekerV2.Mode;
 
-public class BeaconSensor extends Sensor
+public class BeaconSensor
 {
 	public final boolean
 		AC = true,
 		DC = false;
-	private final byte
-		//DC_DIR_ADDRESS = 0x42,
-		AC_DIR_ADDRESS = 0x49,
-		//DC_SIGNALS_ADDRESS = 0x43,
-		AC_SIGNALS_ADDRESS = 0x4A;
+	private IRSeekerV2 seeker;
+	private boolean mode;
 	
-	byte[] ac_signals;
-	byte[] dc_signals;
-	byte ac_dir;
-	//byte dc_dir;
-	
-	public BeaconSensor(int readBufferAddress, int superproAddress, SensorPort port)
-	{
-		super(readBufferAddress, 0x4E, superproAddress, port);
-		
-		ac_signals = new byte[5];
-		//dc_signals = new byte[5];
-	}
-
-	@Override
-	protected void UpdateData()
-	{
-		byte[] buffer = this.GetReadBuffer();
-		//dc_dir = buffer[DC_DIR_ADDRESS];
-		ac_dir = buffer[AC_DIR_ADDRESS];
-		for (int i = 0; i < 5; i++)
-		{
-			//dc_signals[i] = buffer[DC_SIGNALS_ADDRESS + i];
-			ac_signals[i] = buffer[AC_SIGNALS_ADDRESS + i];
-		}
+	public BeaconSensor(SensorPort port, boolean mode)
+	{		
+		this.mode = mode;
+		seeker = new IRSeekerV2(port, getLejosMode(mode));
 	}
 	
-	public int getDirection()
+	private Mode getLejosMode(boolean mode)
 	{
-		return ac_dir - 5;
+		return mode == AC ? Mode.AC : Mode.DC;
+	}
+	
+	public void setMode(boolean mode)
+	{
+		this.mode = mode;
+		seeker.setMode(getLejosMode(mode));
+	}
+	public void invertMode()
+	{
+		setMode(!mode);
+	}
+	public boolean getMode()
+	{
+		return mode;
+	}
+	
+	public float getAngle()
+	{
+		return seeker.getAngle();
 	}
 }
