75342852da9c8766e61045265cb7c883b2b62b40
jashaszun123@gmail.com <jashaszun123@gmail.com@1893abae-4db6-8699-72c5-db63c887fb1e>
2013-04-11 00:51:37 +0000

    Created an UrbanChallenge file for the (you guessed it!) Urban Challenge.

---
+++ year2013/UrbanChallenge.java
@@ -0,0 +1,28 @@
+package year2013;
+
+import lejos.nxt.LCD;
+import year2013.NXTApp.NXTApp;
+
+public class UrbanChallenge extends NXTApp
+{
+	
+	protected UrbanChallenge()
+	{
+		super(50);
+	}
+
+	protected void Update()
+	{
+		LCD.drawString("Welcome to the\nUrban Challenge!\nPress any button\nto win.", 0, 0);
+	}
+
+	protected boolean ShouldExit()
+	{
+		return Button.Left.IsDown() || Button.Right.IsDown() || Button.Enter.IsDown() || Button.Escape.IsDown();
+	}
+
+	public static void main(String[] args)
+	{
+		new UrbanChallenge().Run();
+	}
+}
