0a09aaf278be0a26abccfa2a595d13c34b981b4a
feralinprog@gmail.com <feralinprog@gmail.com@1893abae-4db6-8699-72c5-db63c887fb1e>
2013-06-12 20:40:29 +0000

    Removed old commented code from Maze.java.

--- year2013/maze/Maze.java
+++ year2013/maze/Maze.java
@@ -35,28 +35,6 @@ public final class Maze
 	public void move(Movement movement)
 	{
 		path.add(movement);
-		
-		Movement L = Movement.TurnLeft,
-				 F = Movement.Forward,
-				 R = Movement.TurnRight;
-
-//		boolean anyChanges;
-//		do
-//		{
-//			// convert:
-//			//   L-R(n)-R-L to (n)-
-//			//   R-L(n)-L-R to (n)-
-////			anyChanges = false;
-////			if (convertEnd(
-////				list(L, F, R, F, R, F, L),
-////				list(F)))
-////				anyChanges = true;
-////			if (convertEnd(
-////				list(R, F, L, F, L, F, R),
-////				list(F)))
-////				anyChanges = true;
-//			anyChanges = pruneEnd();
-//		} while (anyChanges);
 		while (pruneEnd()) ;
 	}
 	
@@ -131,72 +109,3 @@ public final class Maze
 		return movements;
 	}
 }
-
-/*
-public class Maze
-{
-	public static void main(String[] args)
-	{
-		Maze maze = new Maze();
-		maze.move(Movement.Forward);
-		maze.move(Movement.TurnLeft);
-		maze.move(Movement.Forward);
-		maze.move(Movement.TurnRight);
-		maze.move(Movement.Forward);
-		maze.move(Movement.TurnRight);
-		maze.move(Movement.Forward);
-		maze.move(Movement.TurnRight);
-		maze.move(Movement.Forward);
-		maze.move(Movement.TurnLeft);
-		maze.move(Movement.Forward);
-		
-		maze.prune();
-		for (Movement movement : maze.getMovements())
-		{
-			System.out.println(movement);
-		}
-	}
-	
-	Path path;
-	Dir dir;
-	
-	public Maze()
-	{
-		dir = null;
-		path = new Path();
-		path.add(new Coord(0, 0));
-	}
-	
-	public void move(Movement movement)
-	{
-		switch (movement)
-		{
-			case Forward:
-				if (dir == null)
-					// any random direction
-					dir = Dir.Right;
-				// add next coordinate
-				path.add(path.getLastCoord().inDir(dir));
-				break;
-			case TurnLeft:
-				dir = dir.rotateLeft();
-				break;
-			case TurnRight:
-				dir = dir.rotateRight();
-				break;
-			default:
-				throw new Error("Should never get here... stupid javacc can't figure out that I have exhausted the pattern match :(");
-		}
-	}
-	
-	public void prune()
-	{
-		path.prune();
-	}
-	
-	public List<Movement> getMovements()
-	{
-		return path.getMovements();
-	}
-}
-*/
